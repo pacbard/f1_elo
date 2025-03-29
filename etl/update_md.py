65d9d5175902102c8ac861674d2e9a49beb948ed
Emanuele Bardelli <bardellie@gmail.com>
2025-03-29 10:24:01 -0700

    Setup github action to rebuild the dataset

---
+++ etl/update_md.py
@@ -0,0 +1,15 @@
+import duckdb
+
+conn = duckdb.connect('md:F1_Results')
+
+conn.execute("""
+attach 'f1_results.duckdb' as f1_local;
+
+use F1_Results;
+
+create or replace table constructor as select * from f1_local.constructor;
+create or replace table driver as select * from f1_local.driver;
+create or replace table elo as select * from f1_local.elo;
+create or replace table race as select * from f1_local.race;
+create or replace table race_result as select * from f1_local.race_result;
+""")
