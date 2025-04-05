b3459043fe378099fa419a01ff8955a85b7dd4ec
Emanuele Bardelli <bardellie@gmail.com>
2025-04-04 19:40:41 -0700

    Fix stuff

--- evidence/sources/f1_results/race.sql
+++ evidence/sources/f1_results/race.sql
@@ -1,5 +1,5 @@
 select 
-    race.*,
+    race.* exclude (pre_qualifying_date, free_practice_4_date, qualifying_1_date, qualifying_2_date, warming_up_date),
     grand_prix.short_name 
 from race
     join grand_prix on grand_prix.id = race.grand_prix_id
